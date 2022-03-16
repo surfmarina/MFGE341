ce1be0b1b2fea1b3ab6b8e6aeb230771e39ef26c
Marina Keller <73908142+surfmarina@users.noreply.github.com>
2022-03-16 14:54:29 -0700

    Add files via upload

---
+++ lab3.asm
@@ -0,0 +1,64 @@
+start:
+		LDI R16, 0xFF
+		OUT DDRB, R16; port B are outputs (LED)
+		LDS R16, 0x0A
+		LDS R17, 0x06
+		ADD R17, R16
+		.EQU N, 0x0100	
+		STS N, R17
+
+l1:
+		LDI R16, 0b00100001
+		OUT PORTB, R16
+		CALL DELAY
+		LDI R16, 0b00000000
+		OUT PORTB, R16
+		CALL DELAY
+		DEC R17
+		BRNE RET
+		LDS R16, 10
+		CALL DELAY
+		DEC R16
+		BRNE fiveseconds
+
+		; subtract the number B from A, and back up to SRAM 0x0100
+		LDS R16, 0x0A
+		LDS R17, 0x06
+		SUB R16, R17
+		.EQU M, 0x0100	
+		STS M, R16
+l2:
+		LDI R16, 0b00100001
+		OUT PORTB, R16
+		CALL DELAY
+		CALL DELAY
+		LDI R16, 0b00000000
+		OUT PORTB, R16
+		CALL DELAY
+		CALL DELAY
+		DEC R17
+		BRNE fiveseconds
+JMP: JMP HERE; end the program
+
+			
+fivesecons:
+		LDI R16, 10; turns led on
+        CALL DELAY
+        DEC R16
+		BRNE RET		 
+DELAY:
+       LDI R21, 20; 21 for 1/2 second loop 
+Loop1:
+       LDI R22, 202
+Loop2:
+       LDI R23, 250
+Loop3:
+       NOP
+       NOP
+       DEC R23
+       BRNE Loop3
+       DEC R22
+       BRNE Loop2
+       DEC R21
+       BRNE Loop1
+       RET
--- main.asm
+++ main.asm
@@ -1,32 +1,43 @@
 start:
-	   inc r16
-	   LDI R16, 0xFF
-       OUT DDRB, R16
-	   LDI R17, 0x0F ; 1111 in binary= 15 in decimal. Max number on LED clock
-	   LDI R20, 0x00 ; starting number, will be updates each iteration and pushed to LED's
-MAIN:
-		CALL BLINK ; 1 second
-		CALL BLINK ; 1 second
-		JMP  MAIN
-		
+    inc r16
+    LDI R16, 0xFF
+		OUT DDRB, R16; port B are outputs (LED)
+	LDI R16, 0x00
+		OUT DDRD, R16; port D are inputs (PUSHBUTTON)
 
-
-
-BLINK: ; entire blink routine takes 1 second
-		LDI R16, 0b0100000
+	; blink once (on 1 sec, off 1 sec)
+	LDI R16, 0b00100001
         OUT PORTB, R16
         CALL DELAY
         LDI R16, 0b00000000
         OUT PORTB, R16
         CALL DELAY
-        RET
 
-DELAY: ; delays for 0.5 seconds
-       LDI R21, 35
+AGAIN:; continuously reads the pushbutton and will only move onto blink when PIND.2 reads as HIGH
+	CBI PIND, 2; clear
+	CALL DELAY; this is necessary to give pin a chance to read a second button press
+	SBIS PIND, 2
+	JMP AGAIN
+	CALL BLINK
+			
+BLINK:
+		LDI R16, 0b00100001; turns led on
+        OUT PORTB, R16
+        CALL DELAY
+        LDI R16, 0b00000000; turns led off
+        OUT PORTB, R16
+		CALL DELAY
+		SBIS PIND, 2
+		JMP BLINK
+		JMP AGAIN
+
+			 
+DELAY:
+       LDI R21, 40
 Loop1:
        LDI R22, 200
 Loop2:
-       LDI R23, 250
+       LDI R23, 248
 Loop3:
        NOP
        NOP
